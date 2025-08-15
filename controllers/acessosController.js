854b6dded6d9c22dfccc340f737b07a56c7f0fdb
Elivelton <eliveltongonzaga@hotmail.com>
2025-08-15 14:38:58 -0300

    update(BaseDados): migrado base de dados para GDFAbrica

--- controllers/acessosController.js
+++ controllers/acessosController.js
@@ -2,6 +2,6 @@ const supabase = require("../client/clientSupabase");
 const { createRpcHandler } = require("./rpcHandlerFactory");
 
 module.exports = {
-  getUserAccess: createRpcHandler("get_acesso", "query"),
   setUserAccess: createRpcHandler("set_acesso", "body"),
+  getUserAccess: createRpcHandler("get_acesso", "query"),
 };
